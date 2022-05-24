869c2a7dbcd4cc03efa6cb4cee87d22e516eb448
Gorbushka3000 <glebdeshar@gmail.com>
2022-05-24 18:22:17 +0300

    Создаем окно

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SFML;
+using SFML.Graphics;
+using System;
+
+namespace MyGame
+{
+    class Program
+    {
+        static RenderWindow win;
+        static void Main(string[] args)
+        {
+            win = new RenderWindow(new SFML.Window.VideoMode(800, 600), "MyGame");
+            win.SetVerticalSyncEnabled(true);
+            win.Closed += Win_Closed;
+            while (win.IsOpen)
+            {
+                win.DispatchEvents();
+                win.Clear(Color.Black);
+                win.Display();
+            }
+        }
+
+        private static void Win_Closed(object? sender, EventArgs e)
+        {
+            win.Close();
+        }
+    }
+}
